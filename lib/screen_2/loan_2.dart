72fb7a6b9de26f9999920fc8a2f45decbbcc4539
Addition <joeladdition@gmail.com>
2025-01-13 13:15:02 +0000

    Changes made

--- lib/screen_2/loan_2.dart
+++ lib/screen_2/loan_2.dart
@@ -9,6 +9,28 @@ class Loan extends StatefulWidget {
 }
 
 class _LoanState extends State<Loan> {
+  TextEditingController _controller1 = TextEditingController();
+  TextEditingController _controller2 = TextEditingController();
+  TextEditingController _controller3 = TextEditingController();
+  String selected = ""; // Default loan period
+  late double totalInterest;
+  late double monthlyInterest;
+  late double monthlyInstallment;
+
+  void loancalculation() {
+    final amount = int.parse(_controller1.text) - int.parse(_controller2.text);
+    final tinterest =
+        amount * (double.parse(_controller3.text) / 100) * int.parse(selected);
+    final minstall = (amount + tinterest) / (int.parse(selected) * 12);
+
+    setState(() {
+      totalInterest = tinterest;
+      monthlyInstallment = minstall;
+      monthlyInterest =
+          tinterest / (int.parse(selected) * 12); // Calculate monthly interest
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -81,11 +103,20 @@ class _LoanState extends State<Loan> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  inputForm(title: "Car Price", hintText: "e.g 10,000,000"),
+                  inputForm(
+                      title: "Car Price",
+                      hintText: "e.g 10,000,000",
+                      controller: _controller1),
                   const SizedBox(height: 20),
-                  inputForm(title: "Down Payment", hintText: "e.g 9000"),
+                  inputForm(
+                      title: "Down Payment",
+                      hintText: "e.g 9000",
+                      controller: _controller2),
                   const SizedBox(height: 20),
-                  inputForm(title: "Interest Rate", hintText: "e.g 5%"),
+                  inputForm(
+                      title: "Interest Rate",
+                      hintText: "e.g 5%",
+                      controller: _controller3),
                   SizedBox(
                     height: 5,
                   ),
@@ -119,17 +150,65 @@ class _LoanState extends State<Loan> {
                   SizedBox(
                     height: 20,
                   ),
-                  Container(
-                    height: 60,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: Colors.yellow,
-                      borderRadius: BorderRadius.circular(25),
-                    ),
-                    child: Center(
-                      child: Text(
-                        "Calculate",
-                        style: GoogleFonts.robotoMono(fontSize: 20),
+                  GestureDetector(
+                    onTap: () {
+                      //print("Calculate Button tapped");
+                      loancalculation();
+                      Future.delayed(Duration.zero);
+                      showModalBottomSheet(
+                        backgroundColor: Colors.grey[400],
+                        isDismissible: false,
+                        isScrollControlled: true,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(30),
+                            topRight: Radius.circular(30),
+                          ),
+                        ),
+                        context: context,
+                        builder: (BuildContext context) {
+                          return SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.5,
+                            width: double.infinity,
+                            child: Padding(
+                              padding: const EdgeInsets.fromLTRB(20, 30, 20, 0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "Result",
+                                    style: GoogleFonts.robotoMono(fontSize: 15),
+                                  ),
+                                  SizedBox(height: 15),
+                                  result(
+                                      title: "Total Interest",
+                                      amount: totalInterest),
+                                  result(
+                                      title: "Monthly Interest",
+                                      amount: monthlyInterest),
+                                  result(
+                                      title: "Monthly Installment",
+                                      amount: monthlyInstallment),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                    //
+                    child: Container(
+                      height: 60,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Colors.yellow,
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Calculate",
+                          style: GoogleFonts.robotoMono(fontSize: 20),
+                        ),
                       ),
                     ),
                   )
@@ -142,22 +221,52 @@ class _LoanState extends State<Loan> {
     );
   }
 
-  Widget loanPeriod(String title) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(0, 2, 20, 0),
-      child: Container(
-        height: 40,
-        width: 40,
-        decoration: BoxDecoration(
-          color: Colors.yellow,
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Center(child: Text(title)),
+  //
+  Widget result({required String title, required double amount}) {
+    return ListTile(
+      title: Text(
+        title,
+        style: GoogleFonts.robotoMono(fontSize: 15),
+      ),
+      trailing: Text(
+        amount.toStringAsFixed(2), // Formats the number to 2 decimal places
+        style: GoogleFonts.robotoMono(fontSize: 15),
       ),
     );
   }
 
-  Widget inputForm({required String title, required String hintText}) {
+  //
+
+  Widget loanPeriod(String title) {
+    return GestureDetector(
+      onTap: () {
+        //debugPrint(title);
+        setState(() {
+          selected = title;
+        });
+      },
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 2, 20, 0),
+        child: Container(
+          height: 40,
+          width: 40,
+          decoration: BoxDecoration(
+            border: title == selected
+                ? Border.all(color: Colors.red, width: 2)
+                : null,
+            color: Colors.yellow,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Center(child: Text(title)),
+        ),
+      ),
+    );
+  }
+
+  Widget inputForm(
+      {required String title,
+      required TextEditingController controller,
+      required String hintText}) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -173,6 +282,7 @@ class _LoanState extends State<Loan> {
             borderRadius: BorderRadius.circular(20),
           ),
           child: TextField(
+            controller: controller,
             decoration: InputDecoration(
               border: const OutlineInputBorder(borderSide: BorderSide.none),
               hintText: hintText,
